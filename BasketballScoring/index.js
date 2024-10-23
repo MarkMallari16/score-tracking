f5168ffb37d40e861dabd53bef74547c769bc247
MarkMallari16 <mallariftw01@gmail.com>
2024-10-23 20:49:48 +0800

    initial commit

---
+++ BasketballScoring/index.js
@@ -0,0 +1,24 @@
+let teamOneScore = parseInt(document.getElementById("teamOneScore").innerHTML, 10);
+let teamTwoScore = parseInt(document.getElementById("teamTwoScore").innerHTML, 10);
+
+function updateScores() {
+    document.getElementById("teamOneScore").innerHTML = teamOneScore;
+    document.getElementById("teamTwoScore").innerHTML = teamTwoScore;
+}
+function countScore(team, count) {
+    if (team == 1) {
+        teamOneScore += count;
+    } else if (team == 2) {
+        teamTwoScore += count;
+    }
+    updateScores();
+}
+function addOnePoint(team) {
+    countScore(team, 1)
+}
+function addTwoPoints(team) {
+    countScore(team, 2)
+}
+function addThreePoints(team) {
+    countScore(team, 3)
+}
